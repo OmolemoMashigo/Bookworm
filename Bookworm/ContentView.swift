0fc3df4ad2e7f60e44470e4404c1e9aedd4d8a86
Omolemo Mashigo <omomashigo@gmail.com>
2025-05-12 21:40:11 +0200

    saving data to Student object using SwiftData

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -6,16 +6,34 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
-    @AppStorage("notes") private var notes = ""
+    @Environment(\.modelContext) var modelContext
+    @Query var students: [Student]
     
     var body: some View {
         NavigationStack{
-            TextField("enter your text", text: $notes, axis: .vertical).textFieldStyle(.roundedBorder)
-                .navigationTitle("Notes")
-                .padding()
+            List(students){ student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar{
+                Button("Add"){
+                    
+                    let firstNames = ["Bukayo", "Jurrien", "David", "Declan", "Gabriel", "Jakub", "William"]
+                    
+                    let lastNames = ["Saka", "Timber", "Raya", "Rice", "Magalaes", "Kiwior", "Saliba"]
+                    
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    
+                    let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+                    modelContext.insert(student)
+                }
+                
+            }
         }
     }
 }
